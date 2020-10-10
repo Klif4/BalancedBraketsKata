b16a2fcc681185db4345eef5773001e994a506e3
klif <klif.cheloul@gmail.com>
2020-10-10 23:22:02 +0200

    [GREEN] Pair of open/closed bracket

--- src/balanced-brackets.ts
+++ src/balanced-brackets.ts
@@ -1,3 +1,6 @@
 export function balancedBrackets(brackets: string) {
-    return '';
+    if (brackets === '') {
+        return '';
+    }
+    return 'OK'
 }
--- test/balanced-brackets.test.ts
+++ test/balanced-brackets.test.ts
@@ -3,3 +3,7 @@ import {balancedBrackets} from "../src/balanced-brackets";
 test('empty brackets', () => {
     expect(balancedBrackets('')).toBe('')
 })
+
+test('pair of open/closed bracket', () => {
+    expect(balancedBrackets('[]')).toBe('OK')
+})
