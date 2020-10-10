092f763f8cd55e6fcf4ce3cfd1b8b45568e0f8cc
klif <klif.cheloul@gmail.com>
2020-10-11 00:21:28 +0200

    Kata

--- src/balanced-brackets.ts
+++ src/balanced-brackets.ts
@@ -1,6 +1,19 @@
-export function balancedBrackets(brackets: string) {
-    if (brackets === '') {
-        return '';
-    }
-    return 'OK'
+export function balancedBrackets(brackets: string): string {
+    if (brackets === '') return '';
+    if (brackets.startsWith('[') && isSymmetrical(brackets)) return 'OK';
+    return 'KO';
 }
+
+const isSymmetrical = (brackets: string):boolean => {
+    const leftSide = brackets.slice(0, brackets.length / 2)
+    const rightSide = brackets.slice(brackets.length / 2, brackets.length)
+    const mirroredRightSide = toMirror(rightSide)
+    return leftSide  === mirroredRightSide;
+}
+
+const toMirror = (brackets: string): string => {
+    return brackets.split('')
+        .reverse()
+        .map(bracket => bracket === '[' ? ']' : '[')
+        .join('');
+};
--- test/balanced-brackets.test.ts
+++ test/balanced-brackets.test.ts
@@ -4,6 +4,48 @@ test('empty brackets', () => {
     expect(balancedBrackets('')).toBe('')
 })
 
-test('pair of open/closed bracket', () => {
+test('pair of open/closed bracket is valid', () => {
     expect(balancedBrackets('[]')).toBe('OK')
 })
+
+test('pair of closed/open bracket is not valid', () => {
+    expect(balancedBrackets('][')).toBe('KO')
+})
+
+test('brackets start with ] is not valid', () => {
+    expect(balancedBrackets(']][[]')).toBe('KO')
+})
+
+
+test('brackets start with ] is not valid', () => {
+    expect(balancedBrackets(']][[]]')).toBe('KO')
+})
+
+test('brackets have odd number of bracket is not valid', () => {
+    expect(balancedBrackets('[[]')).toBe('KO')
+})
+
+test('no symmetrical brackets is valid', () => {
+    expect(balancedBrackets('[[]]')).toBe('OK')
+})
+
+test('random test', () => {
+    expect(balancedBrackets('[[][]]')).toBe('OK')
+})
+
+test('random test', () => {
+    expect(balancedBrackets('[[[][]]]')).toBe('OK')
+})
+
+test('random test', () => {
+    expect(balancedBrackets('[[[[]]]')).toBe('KO')
+})
+
+test('random test', () => {
+    expect(balancedBrackets('[][]]')).toBe('KO')
+})
+
+
+
+
+
